c50f8ae3a270afaae6f7831cd7d5032275a74f6d
WalkerRout <89273280+WalkerRout@users.noreply.github.com>
2022-11-22 13:08:12 -0800

    Changed whitespace formatting

--- LinRegression.hs
+++ LinRegression.hs
@@ -1,7 +1,6 @@
 factored :: [Double] -> Double
 factored xs = (fromIntegral (-2)) / (fromIntegral $ length xs)
 
-
 mGradient :: [Double] -> [Double] -> Double -> Double -> Double
 mGradient xs ys m b = (factored xs) * (diff xs ys 0)
   where
@@ -10,7 +9,6 @@ mGradient xs ys m b = (factored xs) * (diff xs ys 0)
     diff (x:xs) (y:ys) acc = 
       let s = x*(y - (m*x + b))
       in diff xs ys (acc+s)
-
 
 bGradient :: [Double] -> [Double] -> Double -> Double -> Double
 bGradient xs ys m b = (factored xs) * (diff xs ys 0)
@@ -21,21 +19,17 @@ bGradient xs ys m b = (factored xs) * (diff xs ys 0)
       let s = (y - (m*x + b))
       in diff xs ys (acc+s)
 
-
 step :: [Double] -> [Double] -> Double -> Double -> Double -> [Double]
 step xs ys m b lr =
   let b' = b - (lr * bGradient xs ys m b)
       m' = m - (lr * mGradient xs ys m b)
   in [m', b']
 
-
 gradientDescent :: [Double] -> [Double] -> Double -> Double -> Double -> Double -> [Double]
 gradientDescent _ _ m b _ 0 = [b, m]
 gradientDescent xs ys m b lr it = 
   let [m', b'] = step xs ys m b lr
   in gradientDescent xs ys m' b' lr (it-1)
-
-
 
 xs = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 ys = [233, 222, 123, 83, 345, 200, 192, 256, 289]
